2b1c005e43ce6edfde562a82b4f0cf44dcb27e19
WilliamHdd <williamhdd@yahoo.fr>
2017-12-13 10:56:16 +0100

    remove useless comments

--- Serial_comm/Serial_comm.ino
+++ Serial_comm/Serial_comm.ino
@@ -1,8 +1,7 @@
 int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
 int photocellReading;    // the analog reading from the sensor divider
-float convert;
 int LEDpin = 11;          // connect Red LED to pin 11 (PWM pin)
-int LEDbrightness;
+
 
 void setup()
 {
@@ -13,16 +12,9 @@ void loop()
 {
   photocellReading = analogRead(photocellPin);
   
-  //convert = (photocellReading / 1023 );
-  //LEDbrightness = convert * 255;
-  
- // photocellReading = 1023 - photocellReading;
-  //LEDbrightness = map(photocellReading, 0, 1023, 0, 255);  
- 
+
   if (Serial.available() > 0)
   {
-   // Serial.println(convert);
-    //Serial.println(photocellReading);
     Serial.println(photocellReading);
     Serial.read();
   }
